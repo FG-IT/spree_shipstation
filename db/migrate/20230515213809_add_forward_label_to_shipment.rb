4c138b134dad07d75dd472a08959867d5aea9698
SmileintheSky <befruitful12@gmail.com>
2023-05-16 08:43:34 -0500

    fix:migration version

--- db/migrate/20230515213809_add_forward_label_to_shipment.rb
+++ db/migrate/20230515213809_add_forward_label_to_shipment.rb
@@ -1,4 +1,4 @@
-class AddForwardLabelToShipment < ActiveRecord::Migration[7.0]
+class AddForwardLabelToShipment < ActiveRecord::Migration[6.1]
   def change
     add_column :spree_shipments, :forward, :boolean
   end
