3d16ce6543774232c35e80d287cba2247e318360
reader <plpm.zhao@gmail.com>
2023-05-16 15:59:52 -0400

    const namescpage

--- app/jobs/spree_shipstation/sync_shipments_cost_job.rb
+++ app/jobs/spree_shipstation/sync_shipments_cost_job.rb
@@ -3,7 +3,7 @@ module SpreeShipstation
     queue_as :default
 
     def perform(shipstation_id)
-      shipstation_account = Spree::ShipstationAccount.find(shipstation_id)
+      shipstation_account = ::Spree::ShipstationAccount.find(shipstation_id)
       SpreeShipstation::ShipmentSyncer.new(shipstation_account).sync
     end
   end
