a09336ec2b465f102ca8ff76690f197d7012f8ca
SmileintheSky <befruitful12@gmail.com>
2023-06-15 16:54:52 -0500

    after work

--- app/jobs/spree_shipstation/sync_shipment_job.rb
+++ app/jobs/spree_shipstation/sync_shipment_job.rb
@@ -2,10 +2,9 @@ module SpreeShipstation
   class SyncShipmentJob < ApplicationJob
     queue_as :default
 
-    def perform
-      ::Spree::ShipstationAccount.active.each do |shipstation_account|
-        SpreeShipstation::ShipmentSyncer.new(shipstation_account).create_shipment_orders
-      end
+    def perform(order_id)
+      shipstation_account = ::Spree::ShipstationAccount.active.first
+      SpreeShipstation::ShipmentSyncer.new(shipstation_account).create_shipment_orders
     end
   end
 end
