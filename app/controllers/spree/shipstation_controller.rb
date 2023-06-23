f7282adb69fe636e6b63941d2013c96285a600f1
SmileintheSky <befruitful12@gmail.com>
2023-06-23 15:13:32 -0500

    remove export

--- app/controllers/spree/shipstation_controller.rb
+++ app/controllers/spree/shipstation_controller.rb
@@ -6,26 +6,6 @@ module Spree
 
     before_action :authenticate_shipstation
 
-    def export
-      store_id = params[:store_id]
-      @shipments = ::Spree::Shipment.joins(:shipstation_order).where(shipstation_order: {needed: true, shipstation_account_id: store_id})
-        .includes([{order: {ship_address: [:state, :country], bill_address: [:state, :country]}, selected_shipping_rate: :shipping_method}, :inventory_units])
-        .between(date_param(:start_date), date_param(:end_date))
-        .order('spree_shipments.created_at desc')
-        .page(params[:page])
-        .per(50)
-
-      shipment_ids = @shipments.map {|shipment| shipment.id }
-      line_item_ids = ::Spree::InventoryUnit.where(shipment_id: shipment_ids).map {|inventory_unit| inventory_unit.line_item_id }
-      @line_items = ::Hash[ ::Spree::LineItem.includes([{variant: [{option_values: :option_type}, :product, :images]}, :refund_items]).where(id: line_item_ids).map do |line_item|
-        [line_item.id, line_item] 
-      end ]
-
-      respond_to do |format|
-        format.xml { render layout: false }
-      end
-    end
-
     def shipnotify
       SpreeShipstation::ShipmentNotice.from_payload(params.to_unsafe_h, request.raw_post).apply
       head :ok
